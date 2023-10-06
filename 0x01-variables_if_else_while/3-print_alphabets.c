8a56543b75dfd1dfb7131568623afe41bff56508
Mohamed Ali <mohammed.ali17955@gmail.com>
2023-10-06 08:56:16 -0700

    Adding 3-print_alphabets.c file

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * Return: is 0 (Success)
+ */
+
+int main(void)
+{
+	int n = 97;
+	int x = 65;
+
+	while (n <= 122 || x <= 90)
+	{
+		if(n <= 122)
+		{
+			printf("%c", n);
+			n++;
+		}
+		else
+		{
+			printf("%c", x);
+			x++;
+		}
+	}
+	printf("\n");
+	return (0);
+}
