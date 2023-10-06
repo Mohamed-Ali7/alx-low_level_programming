53bbb36e3ea339514c5e7fcd2622cfdd48b4c418
Mohamed Ali <mohammed.ali17955@gmail.com>
2023-10-06 09:11:45 -0700

    Adding 4-print_alphabt.c file

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * Return: is 0 (Success)
+ */
+
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		if (n == 101 || n == 113)
+		{
+			n++;
+			continue;
+		}
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
+
