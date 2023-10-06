fb293dbd39f2c231089b1a33eaea3d1c1b5fdbce
Mohamed Ali <mohammed.ali17955@gmail.com>
2023-10-06 09:35:07 -0700

    Adding 7-print_tebahpla.c file

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * Return: is 0 (Success)
+ */
+
+int main(void)
+{
+	int n = 122;
+
+	while (n >= 97)
+	{
+		putchar(n);
+		n--;
+	}
+	putchar('\n');
+	return (0);
+}
+
