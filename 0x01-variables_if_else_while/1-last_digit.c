80d0a3abf2ed7e5fba7b829b66906af039cb63ef
Mohamed Ali <mohammed.ali17955@gmail.com>
2023-10-06 08:41:58 -0700

    Updating 1-last_digit.c file for the fifth time to match Betty coding style

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,7 +13,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	lastDigit = n % 10;
+	lstDigit = n % 10;
 	if (lstDigit > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, lstDigit);
