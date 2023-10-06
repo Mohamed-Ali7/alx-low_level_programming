55ff4b8fa2876f8864fb02de407e8394914c75f8
Mohamed Ali <mohammed.ali17955@gmail.com>
2023-10-06 07:40:00 -0700

    Updating 0-positive_or_negative.c file to match Betty coding style

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,7 +4,7 @@
 
 /**
  * main - entry point
- * Return : is 0 (Success)
+ * Return: is 0 (Success)
  */
 
 int main(void)
@@ -13,17 +13,17 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
-	if(n > 0){
+	if (n > 0)
+	{
 		printf("%d is positive\n", n);
 	}
-	else if(n == 0)
+	else if (n == 0)
 	{
-                printf("%d is zero\n", n);
+		printf("%d is zero\n", n);
         }
 	else
 	{
-                printf("%d is negative\n", n);
+		printf("%d is negative\n", n);
         }
 	return (0);	
 }
