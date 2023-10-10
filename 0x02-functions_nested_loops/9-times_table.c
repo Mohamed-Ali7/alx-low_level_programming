dbbbf8a5148130adccef30d848deb5df9fa6d8cf
Mohamed Ali <mohammed.ali17955@gmail.com>
2023-10-10 15:49:35 -0700

    Updating 9-times_table.c to match Betty coding style

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -22,7 +22,7 @@ void times_table(void)
 				_putchar('0' + (tableNums / 10));
 			}
 			_putchar('0' + (tableNums % 10));
-			if (repeat != 9 || counter != 9)
+			if (repeat != 9)
 			{
 				_putchar(',');
 				_putchar(' ');
