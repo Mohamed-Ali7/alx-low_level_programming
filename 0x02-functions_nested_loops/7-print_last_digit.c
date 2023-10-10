32ac34b21413893c6deab8740995b3fd2a7dc8bd
Mohamed Ali <mohammed.ali17955@gmail.com>
2023-10-10 14:53:31 -0700

    Adding 7-print_last_digit.c file

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_last_digit -  prints the last digit of an
+ * @n: is the number to return its last digit
+ *
+ * Return: the last digit of the recieved number
+ */
+int print_last_digit(int n)
+{
+	int lstDigit = n % 10;
+
+	if (lstDigit < 0)
+        {
+                lstDigit *= -1;
+        }
+	_putchar('0' + lstDigit);
+	return (lstDigit);
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -34,3 +34,8 @@
 **File  `6-abs.c`:** Write a function that computes the absolute value of an integer.
 
 -   Prototype:  `int _abs(int);`
+
+**File  `7-print_last_digit.c`:** Write a function that prints the last digit of a number.
+
+-   Prototype:  `int print_last_digit(int);`
+-   Returns the value of the last digit
