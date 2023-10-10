0a0f8010994af21c62c2f58fb5ef34b2dcdc5b6e
Mohamed Ali <mohammed.ali17955@gmail.com>
2023-10-10 16:31:21 -0700

    Updating 11-print_to_98.c file

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -18,7 +18,7 @@ void print_to_98(int n)
 			_putchar('-');
 			if (n <= -10)
 			{
-				_putchar('0' + ((n / 10) * -1);
+				_putchar('0' + ((n / 10) * -1));
 			}
 			_putchar('0' + ((n % 10) * -1));
 		}
