d53edc000f7f929f948e3364ce646c7f100a2f36
Mohamed Ali <mohammed.ali17955@gmail.com>
2023-10-10 12:36:36 -0700

    Updting 1-alphabet file to match Betty coding style

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,14 @@
 #include "main.h"
 
 /**
- * print_alphabet - writes the character c to stdout
- * @c: The character to print
+ * print_alphabet - writes the alphabet characters in lowercase to stdout
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: void
  */
 void print_alphabet(void)
 {
 	char c = 'a';
+
 	while (c <= 'z')
 	{
 		_putchar(c);
