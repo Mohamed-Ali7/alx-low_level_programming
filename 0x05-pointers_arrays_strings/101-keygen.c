a9011cc0c3ac6c450aa0b63c84af4f8827914537
Mohamed Ali <mohammed.ali17955@gmail.com>
2023-10-16 17:41:27 -0700

    Updating 101-keygen.c file

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -8,33 +8,17 @@
  * Retrun: 0 
  */
 
-void shuffle(char *array, size_t n) {
-	if (n > 1)
-       	{
-		size_t i;
-		for (i = 0; i < n - 1; i++) {
-			size_t j = i + rand() / (RAND_MAX / (n - i) + 1);
-			char t = array[j];
-			array[j] = array[i];
-			array[i] = t;
-		}	
-	}
-}
-
 int main(void)
 {
-	int i = 0;
+	int calc = 0;
 	char c;
-	char characters[] =
-	"AaDFSE105w45S7ssbc3defghi7756j53kl@X77A57F257mnow7W5V4Gpqrst9uvw57sXzx11828xyz3ABCDEFGHIJKLMNO8P8QR4S19TU1VW2XYZ0123456789!@#$%&";
 	srand(time(NULL));
-	shuffle(characters, sizeof(characters) - 1);
-
-	while (i < 100)
+	while (calc < 2645)
 	{
-		c = characters[rand() % (sizeof(characters) - 1)];
+		c = rand() % 128;
+		calc += c;
 		putchar(c);
-		i++;
 	}
+	putchar(2772 - calc);
 	return (0);
 }
