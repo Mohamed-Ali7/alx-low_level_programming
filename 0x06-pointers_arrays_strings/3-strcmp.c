1117a53b12ca6e77e7d8c2d2cec1126e43d6ea36
Mohamed Ali <mohammed.ali17955@gmail.com>
2023-10-18 09:34:02 -0700

    Making updates to some files to match Betty coding style

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,5 +1,5 @@
 /**
- * *_strcat - comapre two strings
+ * *_strcmp - comapre two strings
  * @s1: is the first string to compare
  * @s2: is the second string to compare
  * Return: 0 if s1 = s2, more than 0 if s1 > s2, or less than 0 if s1 < s2
@@ -9,7 +9,7 @@ int _strcmp(char *s1, char *s2)
 	int result = 0;
 	int i = 0;
 
-	while(s1[i] != '\0' || s2[i] != '\0')
+	while (s1[i] != '\0' || s2[i] != '\0')
 	{
 		result = s1[i] - s2[i];
 		if (result != 0)
