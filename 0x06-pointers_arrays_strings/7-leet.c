76a59ac3c979533df37f7bfd97876cc2d41b25f5
Mohamed Ali <mohammed.ali17955@gmail.com>
2023-10-18 07:51:13 -0700

    Adding 7-leet.c file

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+/** *leet - encodes a string into 1337.
+ * @s: is the string to encode
+ * Return: a pointer to the resulting string
+ */
+char *leet(char *s)
+{
+	char *encode = "4aA3eE0oO7tT1lL";
+	int i = 1;
+	int r = 0;
+
+	while (i < 14)
+	{
+		r = 0;
+		while (s[r] != '\0')
+		{
+			if (s[r] == encode[i] || (s[r]) == encode[i + 1])
+			{
+				s[r] = encode[i-1];
+			}
+			r++;
+		}
+		i = i + 3;
+	}
+	return (s);
+}
