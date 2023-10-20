6d30dff41e57d17bacd512a417e931a2d4bc8575
Mohamed Ali <mohammed.ali17955@gmail.com>
2023-10-19 18:00:13 -0700

    Adding 104-print_buffer.c file

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+/**
+ * print_buffer - prints a buffer.
+ * @b: is the buffer (string) to print
+ * @size: is the number of bytes that it will be printed
+ * Return: void
+ */
+
+void print_buffer(char *b, int size)
+{
+	int i = 0;
+	char tmp[10];
+	int x = 0;
+
+	if (size <= 0)
+	{
+		printf("\n");
+		return;
+	}
+	while (i < size)
+	{
+		printf("%08x: ", i);
+		for (x = 0; x < 10; x++)
+		{
+			if (x % 2 == 0 && x > 0)
+			{
+				printf(" ");
+			}
+			if (i < size)
+			{
+				printf("%02x", b[i]);
+				if (b[i] >= 32 && b[i] <= 126)
+				{
+					tmp[x] = b[i];
+				}
+				else
+				{
+					tmp[x] = '.';
+				}
+			}
+			else
+			{
+				printf("  ");
+				tmp[x] = ' ';
+			}
+				i++;
+		}
+		printf(" %s\n", tmp);
+	}
+}
