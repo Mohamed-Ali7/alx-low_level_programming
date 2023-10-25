04bc511b44a73cd02de6f4ca41d8d27c74681f71
Mohamed Ali <mohammed.ali17955@gmail.com>
2023-10-25 15:07:08 -0700

    Adding 5-sqrt_recursion.c file

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,4 +1,5 @@
-#include <stdio.h>
+int _sqrt(int start, int end, int num);
+
 /**
  * _sqrt_recursion -  returns the natural square root of a number.
  * @n: Is the number to get its natural square root
@@ -7,19 +8,36 @@
 
 int _sqrt_recursion(int n)
 {
-	int root = 1;
+	return (_sqrt(1, n, n));
+}
 
-	if (n < 0) 
+/**
+ * _sqrt - a helper function to get the natural square root of a number.
+ * @start: Is the start point to calculate the middle of the number
+ * @end: Is the end point to calculate middle of the number
+ * @num: Is the number to calculate its square root
+ * Return: The natural square root of (num).
+ */
+
+int _sqrt(int start, int end, int num)
+{
+	int mid = (end + start) / 2;
+
+	if (start > end)
 	{
 		return (-1);
 	}
-	
-	if (n == 0 || n == 1)
-	{
-		return n;
-	}
 
-	root += _sqrt_recursion(n - 1);
-	printf("N = %d     ROOT = %d\n", n, root);
-	return root;
+	if (mid * mid == num)
+	{
+		return (mid);
+	}
+	else if (mid * mid < num)
+	{
+		return (_sqrt(mid + 1, end, num));
+	}
+	else
+	{
+		return (_sqrt(start, mid - 1, num));
+	}
 }
