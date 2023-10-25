a61dfe327e4096f1c73cc023b0c99d4679f279ce
Mohamed Ali <mohammed.ali17955@gmail.com>
2023-10-25 10:20:22 -0700

    Updating 6-is_prime_number.c to match Betty coding style

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,3 +1,8 @@
+/**
+ * is_prime_number -returns 1 if a number is a prime number otherwise return 0
+ * @n: is the number to check
+ * Return: 1 if the number (n) is a prime number otherwise return 0
+ */
 int is_prime_number(int n)
 {
 	if (n < 2)
@@ -8,16 +13,16 @@ int is_prime_number(int n)
 	{
 		if (((n % 10) * (n % 10)) == n)
 		{
-			return 0;
+			return (0);
 		}
 	}
 	if (n % 2 != 0 && n != 9)
 	{
-		return 1;
+		return (1);
 	}
 	else
 	{
-		return 0;
+		return (0);
 	}
 	is_prime_number(n - 1);
 	return (0);
