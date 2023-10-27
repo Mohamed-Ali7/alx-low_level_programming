1ca3f29ebbfdcd9f408487a580d112fa10da8d1d
Mohamed Ali <mohammed.ali17955@gmail.com>
2023-10-27 16:24:12 -0700

    Updating main.h file

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,4 +1,3 @@
-#ifndef MAIN_H
 #define MAIN_H
 
 int _putchar(char c);
@@ -22,4 +21,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif 
+#endif /*MAIN_H*/
