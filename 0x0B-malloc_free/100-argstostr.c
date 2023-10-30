f5921de2cc0ebf54e1137916a9f9b1bed4011ab6
Mohamed Ali <mohammed.ali17955@gmail.com>
2023-10-30 15:24:17 -0700

    Adding 100-argstostr.c file

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,49 @@
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdio.h>
+/**
+ * *argstostr - concatenates all the arguments of the program.
+ * @ac: Is the number of the arguments
+ * @av: Is the program arguments which is array of strings
+ * Return: Returns a pointer to a new string, or NULL if it fails
+ */
+
+char *argstostr(int ac, char **av)
+{
+	char *args;
+	int len = 0;
+	int i;
+	int r;
+	int x = 0;
+
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < ac; i++)
+	{
+		r = 0;
+		while (av[i][r] != '\0')
+		{
+			len++;
+			r++;
+		}
+	}
+
+	args = malloc(sizeof(char) * len);
+
+	for (i = 0; i < ac; i++)
+	{
+		r = 0;
+		while (av[i][r] != '\0')
+		{
+			args[x] = av[i][r];
+			x++;
+			r++;
+		}
+		args[x] = '\n';
+		x++;
+	}
+	return (args);
+}
--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -31,3 +31,10 @@ FYI: The standard library provides a similar function:  `strdup`. Run  `man strd
 
 -   Prototype:  `void free_grid(int **grid, int height);`
 -   Note that we will compile with your  `alloc_grid.c`  file. Make sure it compiles.
+
+**File - `100-argstostr.c`:** Write a function that concatenates all the arguments of your program.
+
+-   Prototype:  `char *argstostr(int ac, char **av);`
+-   Returns  `NULL`  if  `ac == 0`  or  `av == NULL`
+-   Returns a pointer to a new string, or  `NULL`  if it fails
+-   Each argument should be followed by a  `\n`  in the new string
