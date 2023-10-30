2a755b77ac9e5dcbc2837e9b637886d8bd024acf
Mohamed Ali <mohammed.ali17955@gmail.com>
2023-10-30 07:32:48 -0700

    Adding 1-strdup.c file

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * *_strdup - Write a function that returns a pointer to a newly allocated
+ * space in memory, which contains a copy of the string given as a parameter.
+ * @str: Is the string to copy
+ * Return: The created array
+ */
+char *_strdup(char *str)
+{
+	char *copiedStr;
+	int len = strlen(str);
+
+	copiedStr = malloc(sizeof(char) * len);
+
+	if (str == NULL || copiedStr == NULL)
+	{
+		return (NULL);
+	}
+
+	while (len >= 1)
+	{
+		copiedStr[len - 1] = str[len - 1];
+		len--;
+	}
+	return (copiedStr);
+}
