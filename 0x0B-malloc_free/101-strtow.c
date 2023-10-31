5efde8ab688bf527207b6f05305e88db1c5d7867
Mohamed Ali <mohammed.ali17955@gmail.com>
2023-10-31 12:11:22 -0700

    Updating 101-strtow.c file for the second time

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -3,35 +3,47 @@
 #include <stdio.h>
 #include <string.h>
 
-char *strFilter(char *s);
+void initArr(char *str, char **strArr, int arrSize);
 /**
  * **strtow - splits a string into words.
  * @str: Is the string to split
  * Return: A pointer to an array of strings (words)
  */
-char **strtow(char *str)
+
+char **strtow (char *str)
 {
 	char **strArr;
 	int arrSize = 1;
+	int i, x;
 	int strLen = 0;
-	int i;
 	int r = 0;
-	char *fltStr;
-	int x = 0;
+	char cur;
+	char next;
 
 	if (str == NULL || str[0] == '\0')
 	{
 		return (NULL);
 	}
-	fltStr = strFilter(str);
-	for (i = 0; fltStr[i] != '\0'; i++)
+
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (fltStr[i] == ' ')
+		cur = str[i];
+		next = str[i + 1];
+		if ((cur <= 32 || cur >= 127) && (!(next > 32) || !(next < 127)))
+		{
+			continue;
+		}
+		else if ((str[0] <= 32 || str[0] >= 127) && r == 0)
+		{
+			r++;
+			continue;
+		}
+		if (str[i] == ' ')
 		{
 			arrSize++;
 		}
 	}
-	if (i == 0;)
+	if (arrSize == 1 && str[0] == ' ')
 	{
 		return (NULL);
 	}
@@ -40,82 +52,89 @@ char **strtow(char *str)
 	{
 		return (NULL);
 	}
-	for (i = 0; i < arrSize; i++)
+	r = 0;
+	i = 0;
+	for (x = 0; x < arrSize; x++)
 	{
 		strLen = 0;
-		while (fltStr[r] != '\0' && fltStr[r] != ' ')
+		while(str[i] != '\0')
 		{
+			cur = str[i];
+			next = str[i + 1];
+			if ((cur <= 32 || cur >= 127) && (!(next > 32) || !(next < 127)))
+			{
+				i++;
+				continue;
+			}
+			else if ((str[0] <= 32 || str[0] >= 127) && r == 0)
+			{
+				i++;
+				r++;
+				continue;
+			}
+			if (str[i] == ' ')
+			{
+				i++;
+				break;
+			}
 			strLen++;
-			r++;
+			i++;
 		}
-		r++;
-		strArr[i] =  malloc(sizeof(char) * (strLen));
-		if (strArr[i] == NULL)
+		strArr[x] = malloc(sizeof(char) * (strLen + 1));
+		if (strArr[x] == NULL)
 		{
 			return (NULL);
 		}
 	}
-	r = 0;
-	for (i = 0; i < arrSize; i++)
-	{
-		x = 0;
-		while (fltStr[r] != '\0' && fltStr[r] != ' ')
-		{
-			strArr[i][x] = fltStr[r];
-			r++;
-			x++;
-		}
-		r++;
-		strArr[i][x] = '\0';
-	}
-	strArr[i] = NULL;
+	initArr(str, strArr, arrSize);
 	return (strArr);
 }
+
 /**
- * *strFilter - Filters a string by removing all un printable characters
- * @s: Is the string to filter
- * Return: A pointer to the new filtered string
+ * **initArr - Initialize the passed array of strings
+ * @str: Is the string which will be used to init the array
+ * Return: A pointer to the initialized array
  */
-char *strFilter(char *s)
+
+void initArr(char *str, char **strArr, int arrSize)
 {
 	int i = 0;
 	int r = 0;
-	int strSize = 0;
+	int x = 0;
+	int j = 0;
 	char cur;
 	char next;
-	char *filteredStr;
 
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		cur = s[i];
-		next = s[i + 1];
-		if ((cur <= 32 || cur >= 127) && (!(next > 32) || !(next < 127)))
-		{
-			continue;
-		}
-		else if ((s[0] <= 32 || s[0] >= 127) && r == 0)
-		{
-			r++;
-			continue;
-		}
-		strSize++;
-	}
-	filteredStr = malloc(sizeof(char) * strSize + 1);
-	r = 0;
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		cur = s[i];
-		next = s[i + 1];
-		if ((cur <= 32 || cur >= 127) && !((next > 32) || !(next < 127)))
-		{
-			continue;
-		}
-		if ((cur <= 32 || cur >= 127) && r == 0)
-		{
-			continue;
-		}
-		filteredStr[r] = cur;
-		r++;
-	}
-	return (filteredStr);
+	for (x = 0; x < arrSize; x++)
+        {
+                j = 0;
+                while(str[i] != '\0')
+                {
+                        cur = str[i];
+                        next = str[i + 1];
+                        if ((cur <= 32 || cur >= 127) && (!(next > 32) || !(next < 127)))
+                        {
+                                i++;
+                                continue;
+                        }
+                        else if ((str[0] <= 32 || str[0] >= 127) && r == 0)
+                        {
+                                i++;
+                                r++;
+                                continue;
+                        }
+                        if (str[i] == ' ')
+                        {
+                                i++;
+				j++;
+                                break;
+                        }
+			strArr[x][j] = str[i];
+			j++;
+                        i++;
+                }
+		strArr[x][j] = '\0';
+        }
+	strArr[x] = NULL;
 }
+
