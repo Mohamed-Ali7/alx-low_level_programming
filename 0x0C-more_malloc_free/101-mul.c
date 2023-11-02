e2cea41856008adbe70e4719cb17e96e401fd452
Mohamed Ali <mohammed.ali17955@gmail.com>
2023-11-02 15:03:46 -0700

    Adding 101-mul.c file

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+void print_number(int n);
+
+/**
+ * _atoi - find digit numbers in a string and resturn them.
+ * @s: is the string to search into
+ * Return: the digit numbers;
+ */
+int _atoi(char *s)
+{
+	int i = 0;
+	int minusChecker = 0;
+	unsigned int integers = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+		{
+			minusChecker++;
+		}
+		if (s[i] >= 48 && s[i] <= 57)
+		{
+			integers *= 10;
+			integers += (s[i] - '0');
+		}
+		else
+		{
+			printf("Error\n");
+			exit(98);
+		}
+		i++;
+	}
+	if (minusChecker % 2 != 0)
+	{
+		integers *= -1;
+	}
+	return (integers);
+}
+/**
+ * main - Entry point
+ * @argc: The number of command line arguments
+ * @argv: An array containing the program command line arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	char *num1;
+	char *num2;
+	unsigned long result;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	num1 = argv[1];
+	num2 = argv[2];
+
+	result = _atoi(num1) * _atoi(num2);
+	print_number(result);
+	return (0);
+}
+
+/**
+ * print_number - prints an integer
+ * @n: is the number to print
+ *
+ * Return: void
+ */
+
+void print_number(int n)
+{
+	unsigned int tmp = n;
+	unsigned int divider = 1;
+
+	if (n < 0)
+	{
+		tmp = (n * -1);
+		_putchar('-');
+	}
+	while (n != 0)
+	{
+		n /= 10;
+		if (n != 0)
+		{
+			divider *= 10;
+		}
+	}
+	while (divider >= 1)
+	{
+		_putchar('0' + ((tmp / divider) % 10));
+		divider /= 10;
+	}
+	_putchar('\n');
+}
