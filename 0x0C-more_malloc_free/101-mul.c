11e7f5f88a02deb81e87c3ba03fc7258fa21c49c
Mohamed Ali <mohammed.ali17955@gmail.com>
2023-11-03 13:57:01 -0700

    Updating 101-mul.c file

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -84,7 +84,7 @@ int main(int argc, char **argv)
 
 char *multiply(char *mainNum, char *subNum, int mainNLen, int subNLen)
 {
-	int resultLen = mainNLen + subNLen;
+	int resultLen = mainNLen + subNLen + 1;
 	char *result = malloc(resultLen);
 	int i = resultLen;
 	int mulReminder, sumReminder;
