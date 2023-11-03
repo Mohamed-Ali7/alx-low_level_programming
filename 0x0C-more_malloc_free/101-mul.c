ad5675a20957a2eff75c4357b96b4c73c39ed9bd
Mohamed Ali <mohammed.ali17955@gmail.com>
2023-11-03 12:39:51 -0700

    Updating 101-mul.c file

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,83 +2,8 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-/**
- * _isdigit - checks if character is digit
- * @c: the character to check
- *
- * Return: 1 if digit, 0 otherwise
- */
-int _isdigit(int c)
-{
-	return (c >= '0' && c <= '9');
-}
 
-/**
- * _strlen - returns the length of a string
- * @s: the string whose length to check
- *
- * Return: integer length of string
- */
-int _strlen(char *s)
-{
-	int i = 0;
-
-	while (*s++)
-		i++;
-	return (i);
-}
-
-/**
- * big_multiply - multiply two big number strings
- * @s1: the first big number string
- * @s2: the second big number string
- *
- * Return: the product big number string
- */
-char *big_multiply(char *s1, char *s2)
-{
-	char *r;
-	int l1, l2, a, b, c, x;
-
-	l1 = _strlen(s1);
-	l2 = _strlen(s2);
-	r = malloc(a = x = l1 + l2);
-	if (!r)
-		printf("Error\n"), exit(98);
-	while (a--)
-		r[a] = 0;
-
-	for (l1--; l1 >= 0; l1--)
-	{
-		if (!_isdigit(s1[l1]))
-		{
-			free(r);
-			printf("Error\n"), exit(98);
-		}
-		a = s1[l1] - '0';
-		c = 0;
-
-		for (l2 = _strlen(s2) - 1; l2 >= 0; l2--)
-		{
-			if (!_isdigit(s2[l2]))
-			{
-				free(r);
-				printf("Error\n"), exit(98);
-			}
-			b = s2[l2] - '0';
-
-			c += r[l1 + l2 + 1] + (a * b);
-			r[l1 + l2 + 1] = c % 10;
-
-			c /= 10;
-		}
-		if (c)
-			r[l1 + l2 + 1] += c;
-	}
-	return (r);
-}
-
-
+char *multiply(char *mainNum, char *subNum, int mainNLen, int subNLen);
 /**
  * main - multiply two big number strings
  * @argc: the number of arguments
@@ -88,27 +13,106 @@ char *big_multiply(char *s1, char *s2)
  */
 int main(int argc, char **argv)
 {
-	char *r;
-	int a, c, x;
+	char *result, *num1, *num2;
+	int num1Len, num2Len;
+	int i = 0;
+	int r = 0;
 
 	if (argc != 3)
-		printf("Error\n"), exit(98);
-
-	x = _strlen(argv[1]) + _strlen(argv[2]);
-	r = big_multiply(argv[1], argv[2]);
-	c = 0;
-	a = 0;
-	while (c < x)
 	{
-		if (r[c])
-			a = 1;
-		if (a)
-			_putchar(r[c] + '0');
-		c++;
+		printf("Error\n");
+		exit(98);
 	}
-	if (!a)
-		_putchar('0');
+	num1 = argv[1];
+	num2 = argv[2];
+	while (num1[i] != '\0' || num2[r] != '\0')
+	{
+		if (num1[i] != '\0')
+		{
+			if (!(num1[i] >= '0' && num1[i] <= '9'))
+			{
+				printf("Error\n");
+				exit(98);
+			}
+			i++;
+		}
+		if (num2[r] != '\0')
+		{
+			if (!(num2[r] >= '0' && num2[r] <= '9'))
+			{
+				printf("Error\n");
+				exit(98);
+			}
+			r++;
+		}
+	}
+	num1Len = strlen(num1);
+	num2Len = strlen(num2);
+	if (num1Len > num2Len)
+	{
+		result = multiply(num1, num2, num1Len, num2Len);
+	}
+	else
+	{
+		result = multiply(num2, num1, num2Len, num1Len);
+	}
+	i = 0;
+	while (result[i] == '0')
+	{
+		i++
+	}
+	while (result[i] != '\0')
+	{
+		_putchar(result[i]);
+		i++;
+	}
 	_putchar('\n');
-	free(r);
+	free(result);
 	return (0);
+}
+
+/**
+ * multiply - Receives two string numbers and multiply them
+ * @mainNum: Is the main number that the sub number will be multiplied to
+ * @subNum: Is the sub number that will be multiplied to the main number
+ * @mainNLen: Is the length of the main number
+ * @subNLen: Is the length of the sub number
+ * Return: A pointer to a string that contains
+ * the result of multipling the two string numbers
+ */
+
+char *multiply(char *mainNum, char *subNum, int mainNLen, int subNLen)
+{
+	int resultLen = mainNLen + subNLen;
+	char *result = malloc(mainNLen + subNLen);
+	int i = resultLen;
+	int mulReminder, sumReminder;
+	int r, tmpR, numR;
+
+	while (i >= 1)
+	{
+		result[i - 1] = '0';
+		i--;
+	}
+	for (i = subNLen - 1; i >= 0; i--)
+	{
+		mulReminder = 0;
+		sumReminder = 0;
+		for (r = mainNLen - 1; r >= 0; r--)
+		{
+			tmpR = (subNum[i] - 48) * (mainNum[r] - 48) + mulReminder;
+
+			numR = (result[i + r + 1] - 48) + (tmpR % 10);
+			result[i + r + 1] = ((numR + sumReminder) % 10) + 48;
+
+			sumReminder = (numR + sumReminder) / 10;
+			mulReminder = tmpR / 10;
+			if (r == 0 && (mulReminder > 0 || sumReminder > 0))
+			{
+
+				result[i + r] = (mulReminder + sumReminder) + 48;
+			}
+		}
+	}
+	return (result);
 }
