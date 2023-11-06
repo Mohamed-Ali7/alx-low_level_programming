593ee5147fbcb42c10760990647466ae543e8bf3
Mohamed Ali <mohammed.ali17955@gmail.com>
2023-11-06 12:19:05 -0800

    Adding 5-free_dog.c file

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - Frees struct of type dog from the memory
+ * @d: Is the struct to free
+ * Return: void
+ */
+
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
