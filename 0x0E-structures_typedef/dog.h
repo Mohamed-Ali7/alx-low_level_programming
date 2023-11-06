b58893d82fe888736c3c92f1e4aa7d96e38bb71b
Mohamed Ali <mohammed.ali17955@gmail.com>
2023-11-06 11:43:59 -0800

    Updating dog.h file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,12 +7,12 @@
  * @owner: The owner of the dog
  * @age: Age of the dog
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	char *owner;
 	float age;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
