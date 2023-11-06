e9b7b73021c7b73fd2a2820294f8c8c2adc62c01
Mohamed Ali <mohammed.ali17955@gmail.com>
2023-11-06 10:28:22 -0800

    Adding dog.h file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Is struct that contains informations about a dog
+ * @name: the name of the dog
+ * @owner: The owner of the dog
+ * @age: Age of the dog
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
+#endif
