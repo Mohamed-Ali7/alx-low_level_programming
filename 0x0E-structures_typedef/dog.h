ac61916bca20b442a8a37a5693527f77b4e278ac
Mohamed Ali <mohammed.ali17955@gmail.com>
2023-11-06 10:45:05 -0800

    Updating 1-init_dog.c file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,3 +16,9 @@ struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
+
+#ifndef DOG_H
+#define DOG_H
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+#endif
