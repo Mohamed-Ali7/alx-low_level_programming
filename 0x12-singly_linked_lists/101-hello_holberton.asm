a5d656600a1a148d34512a98a71c95d80c14712e
Mohamed Ali <mohammed.ali17955@gmail.com>
2023-11-16 14:53:23 -0800

    Updating 101-hello_holberton.asm file

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,11 +1,11 @@
 section .text
-global _start
+global main
 
-_start:
+main:
 	mov rax, 1
 	mov rdi, 1
 	mov rsi, message
-	mov rdx, 16
+	mov rdx, 17
 	syscall
 
 	mov rax, 60
@@ -13,4 +13,4 @@ _start:
 	syscall
 
 section .data
-message: db "Hello, Holberton\n", 0
+message: db "Hello, Holberton", 10
