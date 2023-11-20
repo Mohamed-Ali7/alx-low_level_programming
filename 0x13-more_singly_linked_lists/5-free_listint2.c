9d6a230cab685eb1f64b485d2e56e5d1c43aa667
Mohamed Ali <mohammed.ali17955@gmail.com>
2023-11-20 14:26:38 -0800

    Updating 5-free_listint2.c file

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -13,18 +13,16 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *prev;
-	listint_t *cur = *head;
 
-	if (head == NULL || *head == NULL)
+	if (head == NULL)
 	{
 		return;
 	}
-	while (cur != NULL)
+	while (*head != NULL)
 	{
-		prev = cur;
-		cur = cur->next;
+		prev = *head;
+		*head = (*head)->next;
 		free(prev);
 	}
-	*head = NULL;
 	head = NULL;
 }
