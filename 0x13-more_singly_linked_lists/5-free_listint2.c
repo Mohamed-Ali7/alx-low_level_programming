2e5de94547a3e126404ca1e15a90cbff738b2eee
Mohamed Ali <mohammed.ali17955@gmail.com>
2023-11-20 13:43:30 -0800

    Adding 5-free_listint2.c file

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * free_listint2 - Frees a list_t list.
+ * @head: Is the current first node of the list
+ * Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *prev;
+
+	while (*head != NULL)
+	{
+		prev = *head;
+		*head = (*head)->next;
+		free(prev);
+	}
+	head = NULL;
+}
