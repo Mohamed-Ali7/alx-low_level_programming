6ba12830cb0fdccc9d538b32fd97a74400b1e0fe
Mohamed Ali <mohammed.ali17955@gmail.com>
2023-11-21 12:19:12 -0800

    Updating 9-insert_nodeint.c file

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,6 +1,7 @@
 #include "lists.h"
 #include <stddef.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * insert_nodeint_at_index - Inserts a new node at a given position
@@ -12,30 +13,39 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *new_node;
-	listint_t *tmp = *head;
+	listint_t *cur = *head;
+	listint_t *prev = *head;
 	unsigned int i = 0;
 
 	if (head == NULL)
 	{
 		return (NULL);
 	}
-	new_node = malloc(sizeof(listint_t));
-	new_node->n = n;
 
-	while (tmp->next != NULL && i < idx - 1)
+	if (idx == 0)
 	{
-		tmp = tmp->next;
+		new_node = malloc(sizeof(listint_t));
+		new_node->n = n;
+		new_node->next = *head;
+		*head = new_node;
+		return (*head);
+	}
+	while (cur != NULL && i < idx)
+	{
+		prev = cur;
+		cur = cur->next;
 		i++;
 	}
 
-	new_node->next = tmp->next;
-	tmp->next = new_node;
-
-	if (i < idx - 1 && (tmp->next == NULL || i > idx - 1))
+	if (i != idx)
 	{
 		return (NULL);
 
 	}
-
+	new_node = malloc(sizeof(listint_t));
+	new_node->n = n;
+        new_node->next = cur;
+	prev->next = new_node;
+	printf("Cur is : %d and new node: %d and IDX: %d\n", cur->n, new_node->n, idx);
 	return (*head);
 }
