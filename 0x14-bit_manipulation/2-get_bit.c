1ea8e14af0c018ee8190de2ad5278288d0c8d1ab
Mohamed Ali <mohammed.ali17955@gmail.com>
2023-11-23 14:00:06 -0800

    Updating 2-get_bit.c file

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -11,7 +11,12 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long x = 0;
 	unsigned int i;
+	unsigned int bits = sizeof(unsigned long int) * 8;
 
+	if (bits < index)
+	{
+		return (-1);
+	}
 	for (i = 0; i <= index; i++)
 	{
 		x = 1 << i;
