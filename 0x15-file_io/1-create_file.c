5550f6d72c69679d2bc0f94a707721762bf577f8
Mohamed Ali <mohammed.ali17955@gmail.com>
2023-11-27 05:21:37 -0800

    Updating 1-create_file.c file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,20 +14,27 @@ int create_file(const char *filename, char *text_content)
 {
 	int n;
 	int fd;
+	int len = 0;
 
 	if (filename == NULL)
 	{
 		return (-1);
 	}
+
+	while (text_content[len] != '\0')
+	{
+		len++;
+	}
+
 	fd = open(filename, O_WRONLY | O_CREAT, 0600);
 
 	if (fd == -1)
 	{
 		return (-1);
 	}
-	n = dprintf(fd, "%s", text_content);
+	n = write(fd, text_content, len);
 
-	if (n < 0)
+	if (n == -1)
 	{
 		return (-1);
 	}
