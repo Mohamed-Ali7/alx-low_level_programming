a6f1a0664be716dc1edfbbefaebd099308edf6a0
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-01-11 15:08:42 +0200

    Updating 7-insert_dnodeint.c file

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -32,7 +32,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		temp = temp->next;
 		index++;
 	}
-	if (idx - 1 != index)
+	if (idx - 1 != index || temp == NULL)
 	{
 		free(new_node);
 		return (NULL);
