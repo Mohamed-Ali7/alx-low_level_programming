aadc7c335d5a10a6ddec6b1e30fdcb9ae4bdbfd6
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-02-02 21:20:18 +0200

    Updating 3-hash_table_set.c file

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -19,24 +19,14 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (new_element == NULL)
 		return (0);
-	if (key == NULL || *key == '\0')
+	if (key == NULL || *key == '\0' || ht == NULL)
 	{
 		free(new_element);
 		return (0);
 	}
 	index = hash_djb2((const unsigned char *) key) % ht->size;
 	new_element->key = strdup(key);
-	if (new_element->key == NULL)
-	{
-		free(new_element);
-		return (0);
-	}
 	new_element->value = strdup(value);
-	if (new_element->value == NULL)
-	{
-		free(new_element), free(new_element->key);
-		return (0);
-	}
 	if (ht->array[index] == NULL)
 	{
 		new_element->next = NULL;
