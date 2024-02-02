f62af8c4ac49e48c286047216157f74ca6d43ca7
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-02-02 17:38:19 +0200

    Updating 0-hash_table_create.c file

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -14,7 +14,14 @@ hash_table_t *hash_table_create(unsigned long int size)
 	unsigned long int i;
 	hash_table_t *table = malloc(sizeof(hash_table_t));
 
+	if (table == NULL)
+		return (NULL);
+
 	table->array = malloc(sizeof(hash_node_t) * size);
+
+	if (table->array == NULL)
+		return (NULL);
+
 	table->size = size;
 
 	for (i = 0; i < size; i++)
