3a5377e06c975002af8449fcd697cdde3d025913
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-02-02 17:36:31 +0200

    Adding 0x1A-hash_tables directory and README.md and 0-hash_table_create.c files

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - Creates a hash table.
+ * @size: Is the size of the hash table
+ * Return: A pointer to the newly created hash table
+ * or NULL if something went wrong
+*/
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned long int i;
+	hash_table_t *table = malloc(sizeof(hash_table_t));
+
+	table->array = malloc(sizeof(hash_node_t) * size);
+	table->size = size;
+
+	for (i = 0; i < size; i++)
+		table->array[i] = NULL;
+
+	return (table);
+}
