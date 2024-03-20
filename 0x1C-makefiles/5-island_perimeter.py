110f170bf383b7f6323b739174a802664c8d654d
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-03-20 23:53:09 +0200

    Updating 5-island_perimeter.py file

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -12,18 +12,18 @@ def island_perimeter(grid):
 
     """
 
-    rectangle_width = 0
+    perimeter = 0
     rows = len(grid)
     columns = len(grid[0])
     for row in range(rows):
         for col in range(columns):
             if grid[row][col] == 1:
-                if col == 0 or grid[row][col - 1] == 0 or col + 1 == columns\
-                        or grid[row][col + 1] == 0:
-                    rectangle_width += 1
-                elif row + 1 == rows or grid[row + 1][col] == 0\
-                        or row == 0 or grid[row - 1][col] == 0:
-                    rectangle_width += 1
-                else:
-                    continue
-    return (rectangle_width + 1) * 2
+                if col == 0 or grid[row][col - 1] == 0:
+                    perimeter += 1
+                if col + 1 == columns or grid[row][col + 1] == 0:
+                    perimeter += 1
+                if row + 1 == rows or grid[row + 1][col] == 0:
+                    perimeter += 1
+                if row == 0 or grid[row - 1][col] == 0:
+                    perimeter += 1
+    return perimeter
