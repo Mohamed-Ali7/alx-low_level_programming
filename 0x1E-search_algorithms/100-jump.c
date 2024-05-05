4e59d1e7b5d2bb166d75e5ff6dae60162d4fb994
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-05-05 11:08:55 +0300

    Updating 100-jump.c file

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -3,6 +3,36 @@
 #include <math.h>
 
 /**
+ * search_block - Searches for a value in a part of an array of integers
+ * @array: Is a pointer to the first element of the array to search in
+ * @start: Is the index to start searching from
+ * @end: Is the index to stop searching at
+ * @size: Is the size of the entire array
+ * @value: Is the value to search for
+ * Return: The first index where value is located
+ */
+
+int search_block(int *array, size_t start, size_t end, size_t size, int value)
+{
+	int index = -1;
+
+	printf("Value found between indexes [%ld] and [%ld]\n", start, end);
+
+	if (end >= size)
+		end = size - 1;
+	for (; start <= end; start++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", start, array[start]);
+		if (array[start] == value)
+		{
+			index = start;
+			break;
+		}
+	}
+	return (index);
+}
+
+/**
  * jump_search - Searches for a value in an array of integers
  * using the Jump search algorithm
  *
@@ -15,7 +45,6 @@
 int jump_search(int *array, size_t size, int value)
 {
 	int index = -1;
-	int found = 0;
 	size_t jump_step = sqrt(size);
 	size_t block_start = 0;
 	size_t block_end = jump_step;
@@ -28,25 +57,15 @@ int jump_search(int *array, size_t size, int value)
 		if (block_end < size)
 		{
 			if (array[block_end] >= value)
-				found = 1;
-		}
-		if (block_end > size || found)
-		{
-			printf("Value found between indexes [%ld] and [%ld]\n",
-				block_start, block_end);
-			if (!found)
-				block_end = size - 1;
-			for (; block_start <= block_end; block_start++)
 			{
-				printf("Value checked array[%ld] = [%d]\n",
-								block_start, array[block_start]);
-				if (array[block_start] == value)
-				{
-					index = block_start;
-					block_start = size;
-					break;
-				}
+				index = search_block(array, block_start, block_end, size, value);
+				break;
 			}
+		}
+		else
+		{
+			index = search_block(array, block_start, block_end, size, value);
+			break;
 		}
 		block_end += jump_step;
 	}
